2234d19faad3e513e50d11b5f805efe3a5ee6a47
sarangsurve <sarangsurve126@gmail.com>
2017-06-29 23:43:35 +0530

    Rock-Paper-Scissor using python3

---
+++ README.md
@@ -0,0 +1,15 @@
+# Rock Paper Scissor
+
+Rock-Paper-Scissor Application based on Python3.
+
+## Requirement
+
+[![python](https://img.shields.io/badge/Python3-3776AB?style=for-the-badge&logo=python&logoColor=white)](https://www.python.org/downloads/release/python-361/)
+
+## Usage
+
+Run Game
+
+```bash
+$ python3 game.py
+```
---
+++ game.py
@@ -0,0 +1,25 @@
+import random
+
+def play():
+    user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissor: ")
+    computer = random.choice(['r', 'p', 's'])
+    if computer == 'r':
+        print('Computer choosed Rock!')
+    elif computer == 'p':
+        print('Computer choosed Paper!')
+    else:
+        print('Computer choosed Scissor!')
+
+    if user == computer:
+        return 'It\'s a tie.'
+
+    if is_win(user, computer):
+        return 'You won!'
+
+    return 'You lost!'
+
+def is_win(player, opponent):
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
+        return True
+
+print(play())
